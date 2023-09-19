69de0be564c4c0a8fb837703a98d8f9110c20d61
Mr. Nikipon <temmmyofficial@gmail.com>
2023-09-20 02:20:00 +0700

    added routes and function checking current user

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
-const { requireAuth } = require('./middleware/authMiddleware');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const app = express();
 
@@ -15,19 +15,37 @@ app.set('view engine', 'ejs');
 
 // database connection
 const dbURI =
-  'mongodb+srv://temmmy:bog132435@users.usjmsq0.mongodb.net/e-commerce?retryWrites=true&w=majority';
+    'mongodb+srv://temmmy:bog132435@users.usjmsq0.mongodb.net/e-commerce?retryWrites=true&w=majority';
 mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+    .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser)
 app.get('/', (req, res) => res.render('homepage'));
 app.get('/about', (req, res) => res.render('about'));
 app.get('/copyright', (req, res) => res.render('copyright'));
 app.get('/privacy', (req, res) => res.render('privacy'));
 app.get('/help', (req, res) => res.render('help'));
+
+app.get('/myAccount', requireAuth, (req, res) => res.render('myAccount'));
+// Customer Pages
 app.get('/productsPage', requireAuth, (req, res) => res.render('customerProducts'));
+app.get('/productDetailPage', requireAuth, (req, res) => res.render('customerProductDetails'));
+
+// Vendor Pages
+app.get('/myProducts', requireAuth, (req, res) => res.render('vendorViewProduts'));
+app.get('/addProducts', requireAuth, (req, res) => res.render('vendorAddProducts'));
+
+
+// Shipper Pages
+app.get('/shipperOrders', requireAuth, (req, res) => res.render('customerProducts'));
+
+
+
+
+
 app.get('/shipperREG', (req, res) => res.render('shipperREG'));
 app.get('/customerREG', (req, res) => res.render('customerREG'));
 app.get('/vendorREG', (req, res) => res.render('vendorREG'));
