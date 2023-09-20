c8bfa4390c132b0cf30e8f00f1f034e6d1870b42
Khoi Tran <khooi.hoodi@gmail.com>
2023-09-20 16:29:43 +0700

    created schema for vendors/products, fixed title name for all pages

--- app.js
+++ app.js
@@ -15,36 +15,31 @@ app.set('view engine', 'ejs');
 
 // database connection
 const dbURI =
-    'mongodb+srv://temmmy:bog132435@users.usjmsq0.mongodb.net/e-commerce?retryWrites=true&w=majority';
+  'mongodb+srv://temmmy:bog132435@users.usjmsq0.mongodb.net/e-commerce?retryWrites=true&w=majority';
 mongoose
-    .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => app.listen(3000))
-    .catch((err) => console.log(err));
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // routes
-app.get('*', checkUser)
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('homepage'));
 app.get('/about', (req, res) => res.render('about'));
 app.get('/copyright', (req, res) => res.render('copyright'));
 app.get('/privacy', (req, res) => res.render('privacy'));
 app.get('/help', (req, res) => res.render('help'));
 
-app.get('/myAccount', requireAuth, (req, res) => res.render('myAccount'));
+app.get('/my-account', requireAuth, (req, res) => res.render('myAccount'));
 // Customer Pages
-app.get('/productsPage', requireAuth, (req, res) => res.render('customerProducts'));
-app.get('/productDetailPage', requireAuth, (req, res) => res.render('customerProductDetails'));
+app.get('/products', requireAuth, (req, res) => res.render('customerProducts'));
+app.get('/product-details', requireAuth, (req, res) => res.render('customerProductDetails'));
 
 // Vendor Pages
-app.get('/myProducts', requireAuth, (req, res) => res.render('vendorViewProduts'));
-app.get('/addProducts', requireAuth, (req, res) => res.render('vendorAddProducts'));
-
+app.get('/my-products', requireAuth, (req, res) => res.render('vendorViewProduts'));
+app.get('/add-products', requireAuth, (req, res) => res.render('vendorAddProducts'));
 
 // Shipper Pages
 app.get('/shipperOrders', requireAuth, (req, res) => res.render('customerProducts'));
-
-
-
-
 
 app.get('/shipperREG', (req, res) => res.render('shipperREG'));
 app.get('/customerREG', (req, res) => res.render('customerREG'));
