3ea012cbfdafcde36043ef6e72db3a5ebf509325
Nguyen Chi Nghia <58958851+temmmy@users.noreply.github.com>
2023-09-25 03:05:53 +0700

    FINAL FINAL VERSION

--- models/Vendor.js
+++ models/Vendor.js
@@ -1,3 +1,15 @@
+// RMIT University Vietnam
+// Course: COSC2430 Web Programming
+// Semester: 2023B
+// Assessment: Assignment 2
+// Authors: 
+// Nguyen Chi Nghia (s3979170)
+// Tran Bao Khoi (s3926093)
+// Tran Hoang Son (s3978450)
+// Bui Cong Duy (s3978546)
+// Hoang Quoc Dat (s3979331)
+// Acknowledgement: https://www.youtube.com/watch?v=SnoAwLP1a-0&list=PL4cUxeGkcC9iqqESP8335DA5cRFp8loyp 
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
--- public/js/customerProducts.js
+++ public/js/customerProducts.js
@@ -1,4 +1,16 @@
-// Filter by name
+// RMIT University Vietnam
+// Course: COSC2430 Web Programming
+// Semester: 2023B
+// Assessment: Assignment 2
+// Authors: 
+// Nguyen Chi Nghia (s3979170)
+// Tran Bao Khoi (s3926093)
+// Tran Hoang Son (s3978450)
+// Bui Cong Duy (s3978546)
+// Hoang Quoc Dat (s3979331)
+// Acknowledgement: https://www.youtube.com/watch?v=SnoAwLP1a-0&list=PL4cUxeGkcC9iqqESP8335DA5cRFp8loyp 
+
+// Search by price
 
 document.addEventListener('DOMContentLoaded', function () {
     const searchInput = document.querySelector('input[type="search"]');
